d2d1d0e156e4e903d7ece24645963025facec10c
Jordan Last <jordan.michael.last@gmail.com>
2015-04-01 17:41:54 -0600

    photos change on type

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ var app = angular.module('aslFingerSpelling', []);
 app.controller('MainController', ['$scope', 
 	function($scope) {
 		
-		$scope.stuff = 'hello';
+		$scope.letters = '';
 		
+		$scope.$watch('letters', function() {
+			
+			if($scope.letters === '') {
+				$scope.imgSrc = 'images/transparent.png';
+				return;
+			}
+			
+			$scope.imgSrc = 'images/' + $scope.letters[$scope.letters.length - 1] + '.png';
+		});
 	}
 ]);
