2f0012c3def3a1a1bbd6af5e0f9860fc2d267c39
Jordan Last <jordan.michael.last@gmail.com>
2015-04-01 19:42:53 -0600

    playing it works!

--- app.js
+++ app.js
@@ -1,19 +1,59 @@
 var app = angular.module('aslFingerSpelling', []);
 
-app.controller('MainController', ['$scope', 
-	function($scope) {
+app.controller('MainController', ['$scope', '$timeout',
+	function($scope, $timeout) {
 		
 		$scope.letters = '';
+		$scope.showPlayButton = false;
 		
+		var currentTimeout;
 		$scope.$watch('letters', function() {
+			$timeout.cancel(currentTimeout);
+			$scope.showPlayButton = false;
 			
-			if($scope.letters === '') {
+			if($scope.letters.trim() === '') {
 				$scope.imgSrc = 'images/transparent.png';
 				return;
 			}
 			
+			currentTimeout = $timeout(function() {
+				if ($scope.letters !== '') {
+					$scope.showPlayButton = true;
+				}
+			}, 1500);
+			
 			var lowercase = $scope.letters.toLowerCase();
 			$scope.imgSrc = 'images/' + lowercase[lowercase.length - 1] + '.png';
 		});
+		
+		$scope.play = function() {
+			
+			$scope.showPlayButton = false;
+			
+			var newLetters = $scope.letters;
+			newLetters = newLetters.toLowerCase();
+			newLetters = newLetters.split(' ').join('');
+			$scope.imgSrc = 'images/' + newLetters[0] + '.png';
+			
+			if (newLetters[1]) {
+				showDelayed(newLetters, 1);
+			}
+		};
+		
+		var returnCount = 0;
+		function showDelayed(newLetters, index) {
+			currentTimeout = $timeout(function() {
+				$scope.imgSrc = 'images/' + newLetters[index++] + '.png';
+				
+				if (newLetters[index]) {
+					showDelayed(newLetters, index);
+				}
+				else {
+					currentTimeout = $timeout(function() {
+						$scope.showPlayButton = true;
+					}, 1500);
+				}
+			}, 1500);
+		}
 	}
 ]);
